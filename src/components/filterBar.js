3a87a445422ef936ca2ed6d42524db1e04619924
selvaganesh26 <selvaganeshmahadevan@gmail.com>
2021-10-13 13:46:26 +0000

    refactored filterBar.

--- src/components/filterBar.js
+++ src/components/filterBar.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-console */
 import { React } from 'react';
 import filterButton from './filterButton';
-const filterBar = () => {
-	const filters = ['All', 'Active', 'Completed'];
+import TodoManager from '../services/todoManager';
+import context from '../core/context';
 
-	return <div> { filters.map(filterButton) }</div>;
+const filters = ['All', 'Active', 'Completed'];
+
+const filterBar = () => {
+	const notodo = TodoManager.getTodoCount(context.state.todos) === 0;
+
+	console.log(TodoManager.getTodoCount(context.state.todos));
+	return notodo
+		? null
+		: <div> { filters.map(filterButton) }</div>;
 };
 
 export default filterBar;
--- src/components/filterButton.js
+++ src/components/filterButton.js
@@ -1,18 +1,12 @@
 import { React } from 'react';
 import context from '../core/context';
-import TodoManager from '../services/todoManager';
 
-const filterButton = (filter) => {
-	const notodo = TodoManager.getTodoCount(context.state.todos) === 0;
-
-	return notodo
-		? null
-		: <div key={ filter }>
-			<button onClick={ () =>
-				context.actions.setFilter(filter) }
-			>
-				{ filter }</button>
-		</div>;
-};
+const filterButton = (filter) =>
+	<div key={ filter }>
+		<button onClick={ () =>
+			context.actions.setFilter(filter) }
+		>
+			{ filter }</button>
+	</div>;
 
 export default filterButton;
