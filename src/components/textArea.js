02f954b11655f41510238d8dcd73e1847addbef6
selvaganesh26 <selvaganeshmahadevan@gmail.com>
2021-10-14 05:25:01 +0000

    Action Keys added.

--- src/components/textArea.js
+++ src/components/textArea.js
@@ -1,11 +1,17 @@
 import { React } from 'react';
 import context from '../core/context';
+
+const actionKeys = {
+	Enter: () => context.actions.addTodo(),
+	Escape: () => context.actions.setInput(''),
+};
 const TextArea = () =>
 	<input
 		type="text"
 		size="20"
 		value={ context.state.input }
 		onChange={ (evt) => context.actions.setInput(evt.target.value) }
+		onKeyUp={ (evt) => actionKeys[evt.code] && actionKeys[evt.code]() }
 	/>;
 
 export default TextArea;
