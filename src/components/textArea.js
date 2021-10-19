390020f91ab8c0c6bf86be3e149b641535a1097a
selvaganesh26 <selvaganeshmahadevan@gmail.com>
2021-10-19 12:58:04 +0000

    Enter and esc key added for funcitoning add and edit button instead.

--- src/components/textArea.js
+++ src/components/textArea.js
@@ -1,17 +1,26 @@
+/* eslint-disable no-console */
 import { React } from 'react';
 import context from '../core/context';
 
+const getEnterkeyAction = () => (context.state.editing
+	? 'editTodo'
+	: 'addTodo');
+
 const actionKeys = {
-	Enter: () => context.actions.addTodo(),
+	Enter: () => context.actions[getEnterkeyAction()](),
 	Escape: () => context.actions.setInput(''),
 };
+
 const TextArea = () =>
 	<input
 		type="text"
 		size="20"
 		value={ context.state.input }
 		onChange={ (evt) => context.actions.setInput(evt.target.value) }
-		onKeyUp={ (evt) => actionKeys[evt.code] && actionKeys[evt.code]() }
+		onKeyUp={ (evt) => {
+			console.log(evt.code);
+			actionKeys[evt.code] && actionKeys[evt.code]();
+		} }
 	/>;
 
 export default TextArea;
