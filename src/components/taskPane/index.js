a71eba26ac780e0a5341da3b73a5c7b719139322
selvaganesh26 <selvaganeshmahadevan@gmail.com>
2021-11-12 14:49:33 +0000

    Added styles class i in CSS.

--- src/components/taskPane/index.js
+++ src/components/taskPane/index.js
@@ -7,7 +7,7 @@ const style = {
 	right: 0,
 	height: '100%',
 	width: '50%',
-	background: 'bisque',
+	background: 'yellow',
 };
 
 const TaskPane = () =>
--- src/components/todoPane/todoDisplay.js
+++ src/components/todoPane/todoDisplay.js
@@ -1,18 +1,23 @@
 import { React } from 'react';
 import context from '../../core/context';
-const todoDisplay = (todo) => <div key={ todo.id }>
-	<input
-		type="checkbox"
-		checked={ todo.isCompleted }
-		onChange={ () => context.actions.toggleTodo(todo) }
-	/>
-	<span onClick={ () => context.actions.setEditing(todo) }>
-		{ todo.text }
-	</span>
-	<span>
-		<button	onClick={ () => context.actions.removeTodo(todo) }>
-			X</button>
-	</span>
-</div>;
+
+const todoDisplay = (todo) => {
+	const className = `todo ${ todo.isCompleted ? 'todo-completed' : 'todo-active' }` ;
+
+	return <div key={ todo.id } className={ className }>
+		<input
+			type="checkbox"
+			checked={ todo.isCompleted }
+			onChange={ () => context.actions.toggleTodo(todo) }
+		/>
+		<span onClick={ () => context.actions.setEditing(todo) }>
+			{ todo.text }
+		</span>
+		<span>
+			<button	onClick={ () => context.actions.removeTodo(todo) }>
+				X</button>
+		</span>
+	</div>;
+};
 
 export default todoDisplay;
